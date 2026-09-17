f9472e3c543dd509ef8af992855ed5c5daee3d5a
Anthony West <anthony.s.west@gmail.com>
2026-09-17 01:10:11 -0700

    Bump base log version to 0.26.2

--- ASWLog/ASWLog_Base.h
+++ ASWLog/ASWLog_Base.h
@@ -57,7 +57,7 @@ protected:
 public:
     std::string_view GetVersionStr() const noexcept final
     {
-        return "0.26.1"; // Semantic Versioning
+        return "0.26.2"; // Semantic Versioning
     }
 
     std::string GetFullVersionStr() const final
